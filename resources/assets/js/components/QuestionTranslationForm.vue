0c9b7fd17c0131684b9cd93956313bb72e565b60
Chabib Nurochman <chabib.nr@gmail.com>
2017-06-06 02:25:56 +0700

    Attemp fix race condition

--- resources/assets/js/components/QuestionTranslationForm.vue
+++ resources/assets/js/components/QuestionTranslationForm.vue
@@ -5,7 +5,7 @@
       .field.four.wide
         label {{ $t('Translate to') }}
         select.language.ui.dropdown(name='language' v-model="payload.language")
-          template(v-if='user.languages.length > 0')
+          template(v-if='isReady')
             option(v-for="language in languages" ":value"="language.code") {{ language.name }}
       .field
         label {{ $t('Question') }}
@@ -33,6 +33,7 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 import http from 'lib/http'
+import _ from 'lodash'
 
 export default {
   data() {
@@ -51,6 +52,17 @@ export default {
       'question',
       'user'
     ]),
+    isReady() {
+      if (this.user.languages.length == 0) {
+        return false
+      }
+
+      if (_.isEmpty(this.question)) {
+        return false
+      }
+
+      return true
+    },
     languages() {
       return this.user.languages.filter(language => {
         return language.code != this.question.language.code
