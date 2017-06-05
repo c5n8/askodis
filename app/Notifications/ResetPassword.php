6df8652f2616b8be5528a406ce8611653bc00afe
Chabib Nurochman <chabib.nr@gmail.com>
2017-06-05 22:53:02 +0700

    Set locale of mail notifications

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -2,13 +2,19 @@
 
 namespace App\Notifications;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Auth\Notifications\ResetPassword as BaseResetPassword;
 
-class ResetPassword extends BaseResetPassword
+class ResetPassword extends BaseResetPassword implements ShouldQueue
 {
+    use Queueable;
+
     function toMail($notifiable)
     {
+        app()->setLocale($notifiable->locale->code);
+
         return (new MailMessage)
             ->line(__('You are receiving this email because we received a password reset request for your account.'))
             ->action(__('Reset Password'), url(config('app.url').route('password.reset', $this->token, false)))
